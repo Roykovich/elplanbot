0c748da49ed1ec5df78bf1060ca0119f81c6959b
Rubén Ortiz <runerd.menethil@gmail.com>
2022-02-11 20:39:47 -0300

    Convert into strings the values of the account array and create an embed Object for the tweet itself

--- src/events/twitter.js
+++ src/events/twitter.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const Twit = require("twit");
 const he = require("he");
 require("dotenv").config();
@@ -6,42 +7,40 @@ const { consumer_key, consumer_secret, access_token, access_token_secret } =
   process.env;
 
 const accounts = [
-  69008563, // F1 official account
-  ,
+  "69008563", // F1 official account
   /* Constructors accounts */ 226087776, // Red Bull
-  108247668, // Ferrari
-  26235265, // Mercedes
-  24871896, // McLaren
-  90636188, // Williams
-  28297965, // Alpine
-  82322622, // AlphaTauri
-  16143542, // Aston Martin,
-  229454266, // Alpha Romeo
-  2303406624, // Haas
-  ,
+  "108247668", // Ferrari
+  "26235265", // Mercedes
+  "24871896", // McLaren
+  "90636188", // Williams
+  "28297965", // Alpine
+  "82322622", // AlphaTauri
+  "16143542", // Aston Martin,
+  "229454266", // Alpha Romeo
+  "2303406624", // Haas
   /* Drivers accounts */ 556260847, // Maxx Verstappen
-  78502161, // Sergio Perez
-  353786894, // Carlos Sainz
-  262230432, // Charles Leclerc
-  213969309, // Lewis Hamilton
-  394892872, // George Russell
-  516464106, // Lando Norris
-  214413743, // Daniel Ricciardo
-  397306167, // Alex Albon
-  193047282, // Nicholas Latifi
-  507672047, // Fernando Alonso
-  490756890, // Esteban Ocon
-  537951506, // Pierre Gasly
-  759658867177299972, // Yuki Tsunoda
-  4201552600, // Lance Stroll
+  "78502161", // Sergio Perez
+  "353786894", // Carlos Sainz
+  "262230432", // Charles Leclerc
+  "213969309", // Lewis Hamilton
+  "394892872", // George Russell
+  "516464106", // Lando Norris
+  "214413743", // Daniel Ricciardo
+  "397306167", // Alex Albon
+  "193047282", // Nicholas Latifi
+  "507672047", // Fernando Alonso
+  "490756890", // Esteban Ocon
+  "537951506", // Pierre Gasly
+  "759658867177299972", // Yuki Tsunoda
+  "4201552600", // Lance Stroll
   // ! Seb doesn't have a twitter account :(
-  1143472657, // Valtteri Bottas
-  1091070048, // Guanyu Zhou
-  902951244549038081, // Mick Schumacher,
-  322593965, // Nikita Mazepin
+  "1143472657", // Valtteri Bottas
+  "1091070048", // Guanyu Zhou
+  "902951244549038081", // Mick Schumacher,
+  "322593965", // Nikita Mazepin
   /* other social media F1 accounts */
-  2755544640, // Victor Abad efeuno,
-  968920691457318914, // Formula_stats history account
+  "2755544640", // Victor Abad efeuno,
+  "968920691457318914", // Formula_stats history account
 ];
 
 module.exports = async (client) => {
@@ -88,5 +87,12 @@ module.exports = async (client) => {
 
   const sendTweetToChannel = (formatedTweet) => {
     const { text, url, name, avatar, image, id, authorURL } = formatedTweet;
+
+    const embed = new MessageEmbed()
+      .setColor("#ffffff")
+      .setDescription(`\n ${text}`)
+      .setThumbnail(avatar)
+      .setAuthor({ name, iconURL: avatar, url: authorURL })
+      .setImage(image);
   };
 };
